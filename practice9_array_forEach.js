a9c9c4af54af6e48147289f87642fe2513c03e9d
Sam River <sam.river@cydeo.com>
2023-06-23 06:51:03 +0300

    frontend practices

--- practice9_array_forEach.js
+++ practice9_array_forEach.js
@@ -1,44 +1,68 @@
-let names = ["James", "Ozzy", "Sam", "Tyson", "Brooks"];
+let names = ["Clark", "Bruce", "Barry", "Diana", "John"];
 
-// function printStuff(stuff){
+// function printStuff(stuff) {
 //     console.log(stuff);
 // }
 
-// const printStuff = stuff => console.log(stuff);
-// names.forEach( printStuff );
+// const printStuff =  stuff => console.log(stuff);
+    
 
-names.forEach(stuff => console.log(stuff));
+// names.forEach(  printStuff  ); 
 
-names.push("Mike");
+names.forEach(stuff => console.log(stuff)); 
+
+names.forEach(stuff =>  console.log(stuff)  ); 
+
+
+names.push("Snow");
 
 console.log(names);
+// [ 'Clark', 'Bruce', 'Barry', 'Diana', 'John', 'Snow' ]
 
-// store the first and second item into a vairable a,b
-let [a, b ] = names;
+// store the first and second item into a variable a, b 
+let [a, b] = names;  
 
 console.log(a + ' has character count of ' + a.length);
+// Clark has character count of 5
 console.log(b + ' has character count of ' + b.length);
+// Bruce has character count of 5
 
-// // create a new empty array wih name charCountArray
-// let charCountArray = [];
-// // go through each and every item in names array
-// for(let each of names){
+
+// // create a new empty array with name charCountArray
+// let charCountArray = []; 
+// // go through each and every item in names array 
+// for (let each of names){
 //     // get the character count and add it into charCountArray using push method
-//     charCountArray.push(each.length);
+//     charCountArray.push(  each.length  ); 
 // }
+/**
+ *  array's map method is a method can transform each element
+ *  so we can get new array with transformed element
+ * it expect you to provide a function that accept 1 parameter
+ * and return a new value of any kind 
+ * easy way to provide such function is using array syntax 
+ *  each => do Something to return a value here
+ * IT HAS NOTHING TO DO WITH THE MAP AS KEY VALUE PAIR)
+ * SEE MAP METHOD AS TRANSFORMER 
+ */
+let charCountArray = names.map(  each => each.length  )
 
-// map method of array, it returns the manipulated item
-let charCountArray = names.map( each => each.length );
 console.log(charCountArray);
 
-let upperCaseNames = names.map( each => each.toUpperCase() );
-console.log(upperCaseNames);
+let upperNames = names.map(each => each.toUpperCase()) 
+console.log(upperNames);
 
-// filter method of array expect you to provide a predicate function
-let filteredNames = names.filter( each => each.length==4 );
 
-let scores = [56, 99, 65, 100, 45];
+// filter method of array expect you to provide a function 
+// that accept 1 param and return boolean value 
 
-let passedScores = scores.filter( each => each>70 );
+let filteredNames = names.filter(each => each.length == 4);
+console.log(filteredNames); 
+
+console.log("--------------");
+
+let scores = [56, 99, 65, 100, 45]; 
+console.log(scores);
+// create a filtered array to only store the numbers greater than 70
+let passedScores = scores.filter(score => score > 70); 
 console.log(passedScores);
-
